53d918cf8417c341e6d6029bd823dabce0daff2f
nuttapol-kor <nuttapol.kor@ku.th>
2023-04-23 20:06:29 +0700

    navigation drawer (home, profile, logout)

--- app/src/main/java/org/classapp/moodpic/Activities/HomeActivity.kt
+++ app/src/main/java/org/classapp/moodpic/Activities/HomeActivity.kt
@@ -1,12 +1,103 @@
 package org.classapp.moodpic.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import org.classapp.moodpic.R
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+
+        // set current user
+        auth = Firebase.auth
+        db = Firebase.firestore
+        var user: FirebaseUser? = auth.currentUser
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        val headerView = navView.getHeaderView(0)
+        val headerUsername = headerView.findViewById<TextView>(R.id.navHeaderUsername)
+        val headerEmail = headerView.findViewById<TextView>(R.id.navHeaderEmail)
+        val profileImage = headerView.findViewById<ImageView>(R.id.navHeaderImage)
+        if (user != null) {
+            var authEmail = user.email
+            db.collection("users").whereEqualTo("email", authEmail).get().addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    headerUsername.text = documents.documents[0].getString("username")
+                    headerEmail.text = documents.documents[0].getString("email")
+                    var imageUrl = documents.documents[0].getString("profile_image")
+                    if (imageUrl != null) {
+                        Glide.with(this).load(imageUrl).apply(RequestOptions.circleCropTransform()).into(profileImage)
+                    }
+                }
+            }.addOnFailureListener{ exception ->
+                Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            R.id.nav_profile -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ProfileFragment()).commit()
+            R.id.nav_logout -> logout()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun logout() {
+        auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
