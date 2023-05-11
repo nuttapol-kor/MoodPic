c1fabc553e37aabda4707b8bd52a33c6c34b0912
nuttapol-kor <nuttapol.kor@ku.th>
2023-05-12 05:20:30 +0700

    update profile fragment

--- app/src/main/java/org/classapp/moodpic/Activities/ProfileFragment.kt
+++ app/src/main/java/org/classapp/moodpic/Activities/ProfileFragment.kt
@@ -1,10 +1,19 @@
 package org.classapp.moodpic.Activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import org.classapp.moodpic.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -35,7 +44,29 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val profileImg = view.findViewById<ImageView>(R.id.profileSecImg)
+        val usernameTxt = view.findViewById<TextView>(R.id.profileUsernameTxt)
+        val emailTxt = view.findViewById<TextView>(R.id.profileEmailTxt)
+
+        var db = Firebase.firestore
+        var auth = Firebase.auth
+
+        val email = auth.currentUser?.email
+        emailTxt.text = email
+        db.collection("users").whereEqualTo("id", auth.currentUser?.uid).get().addOnSuccessListener {
+            if (!it.isEmpty) {
+                usernameTxt.text = it.documents[0].getString("username")
+                var imageUrl = it.documents[0].getString("profileImageUrl")
+                if (imageUrl != null) {
+                    Glide.with(this).load(imageUrl).apply(RequestOptions.circleCropTransform()).into(profileImg)
+                }
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(requireContext(), exception.message, Toast.LENGTH_SHORT).show()
+            Log.e("getCommentFromFirebase", "Error getting comments", exception)
+        }
+        return view
     }
 
     companion object {
